e6cfaaa3896d350e95c95f5a939d40fb61e57e83
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-13 16:10:07 +0200

    prep changes

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -2,11 +2,17 @@ WITH temp_daily AS (
     SELECT * 
     FROM {{ref('staging_weather')}}
 ),
-add_weekday AS (
+add_columns AS (
     SELECT *,
         date AS weekday,
-        date AS day_num
+        date AS day_num,
+        CASE
+            WHEN EXTRACT(MONTH FROM date) IN (3, 4, 5) THEN 'Spring'
+            WHEN EXTRACT(MONTH FROM date) IN (6, 7, 8) THEN 'Summer'
+            WHEN EXTRACT(MONTH FROM date) IN (9, 10, 11) THEN 'Autumn'
+            ELSE 'Winter'
+        END AS season
     FROM temp_daily
 )
 SELECT *
-FROM add_weekday
+FROM add_columns
