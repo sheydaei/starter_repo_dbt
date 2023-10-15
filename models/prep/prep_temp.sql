068730566acf00f7f2e23ccdf36be78c3e2043c6
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-15 12:17:50 +0200

    prep change

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -7,23 +7,16 @@ add_columns AS (
         date AS weekday,
         date AS day_num,
         CASE
-            WHEN country = 'Australia' 
-                CASE
-                    WHEN EXTRACT(MONTH FROM date) IN (3, 4, 5) THEN 'Autumn'
-                    WHEN EXTRACT(MONTH FROM date) IN (6, 7, 8) THEN 'Winter'
-                    WHEN EXTRACT(MONTH FROM date) IN (9,10,11) THEN 'Spring'
-                    WHEN EXTRACT(MONTH FROM date) IN (12, 1, 2) THEN 'Summer'
-                END
-            ELSE
-                CASE
-                    WHEN EXTRACT(MONTH FROM date) IN (3, 4, 5) THEN 'Spring'
-                    WHEN EXTRACT(MONTH FROM date) IN (6, 7, 8) THEN 'Summer'
-                    WHEN EXTRACT(MONTH FROM date) IN (9, 10, 11) THEN 'Autumn'
-                    WHEN EXTRACT(MONTH FROM date) IN (12, 1, 2) THEN 'Winter'
-                END
+            WHEN country = 'Australia' AND EXTRACT(MONTH FROM date) IN (3, 4, 5) THEN 'Autumn'
+            WHEN country = 'Australia' AND EXTRACT(MONTH FROM date) IN (6, 7, 8) THEN 'Winter'
+            WHEN country = 'Australia' AND EXTRACT(MONTH FROM date) IN (9, 10, 11) THEN 'Spring'
+            WHEN country = 'Australia' AND EXTRACT(MONTH FROM date) IN (12, 1, 2) THEN 'Summer'
+            WHEN country != 'Australia' AND EXTRACT(MONTH FROM date) IN (3, 4, 5) THEN 'Spring'
+            WHEN country != 'Australia' AND EXTRACT(MONTH FROM date) IN (6, 7, 8) THEN 'Summer'
+            WHEN country != 'Australia' AND EXTRACT(MONTH FROM date) IN (9, 10, 11) THEN 'Autumn'
+            WHEN country != 'Australia' AND EXTRACT(MONTH FROM date) IN (12, 1, 2) THEN 'Winter'
         END AS season
     FROM temp_daily
 )
 SELECT add_columns.*
-FROM add_columns
-
+FROM add_columns;
