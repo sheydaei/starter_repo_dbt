1706f5d4b6cd85c5850ab73d50c830053a1c3ba7
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-17 12:04:47 +0200

    city avg change

--- models/mart/city_avg_temp.sql
+++ models/mart/city_avg_temp.sql
@@ -1,10 +1,9 @@
 WITH city_avg AS (
     SELECT 
         city,
-        date,
         AVG(avgtemp_c) AS avg_weekday
     FROM {{ref('prep_temp')}} 
-    GROUP BY city, date
+    GROUP BY city
 )
 
 SELECT *
