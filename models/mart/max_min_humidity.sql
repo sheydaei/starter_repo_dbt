6559646b4d04f459463f4417a4e36979caed3457
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-18 21:37:52 +0200

    humidity change

--- models/mart/max_min_humidity.sql
+++ models/mart/max_min_humidity.sql
@@ -3,10 +3,11 @@ WITH total_precip_humidity AS (
         avghumidity,
         country,
         city,
+        date,
         EXTRACT(YEAR FROM date) AS year,
         SUM(totalprecip_mm) AS yearly_precipitation
     FROM {{ref('prep_temp')}} 
-    GROUP BY city, country, avghumidity,totalprecip_mm,year
+    GROUP BY city, country, avghumidity,totalprecip_mm,year,date
     ORDER BY city
 )
 
