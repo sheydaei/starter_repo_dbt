753217f2186f859d7e04cb3f07c997b79a2cb491
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-19 12:38:34 +0200

    change max min temp

--- models/mart/max_min_temp_yearly.sql
+++ models/mart/max_min_temp_yearly.sql
@@ -1,14 +1,12 @@
 WITH max_min_yearly AS (
     SELECT 
-        maxtemp_c,
-        mintemp_c,
         country,
         city,
         EXTRACT(YEAR FROM date) AS year,
         MAX(maxtemp_c) AS max_maxtemp,
         MIN(mintemp_c) AS min_mintemp
     FROM {{ref('prep_temp')}} 
-    GROUP BY year, country, city, maxtemp_c, mintemp_c
+    GROUP BY year, country, city
     ORDER BY country
 )
 
