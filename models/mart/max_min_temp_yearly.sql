79e0cee9821779ae263a419e8147ed5b16d9c973
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-19 12:46:14 +0200

    file changes

--- models/mart/max_min_temp_yearly.sql
+++ models/mart/max_min_temp_yearly.sql
@@ -2,7 +2,6 @@ WITH max_min_yearly AS (
     SELECT 
         country,
         city,
-        EXTRACT(YEAR FROM date) AS year,
         MAX(maxtemp_c) AS max_maxtemp,
         MIN(mintemp_c) AS min_mintemp
     FROM {{ref('prep_temp')}} 
