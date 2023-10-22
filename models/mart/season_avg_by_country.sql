2cedc0ae80ab8ad87473e05d375924a60434fca7
sheydaei <sheyda.sheydayi95@gmail.com>
2023-10-22 18:35:19 +0200

    mart changes

--- models/mart/season_avg_by_country.sql
+++ models/mart/season_avg_by_country.sql
@@ -2,10 +2,11 @@ WITH season_avgtem_cntry AS (
     SELECT 
         season,
         country,
+        city,
         AVG(avgtemp_c) AS season_avgtemp
     FROM {{ref('prep_temp')}} 
     GROUP BY season, country
-    ORDER BY country
+    ORDER BY country,city
 )
 
 SELECT *
